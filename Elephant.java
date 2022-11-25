9bbeb82007651e14a0291d8877c0272856408c73
POKEstepheN <348921842@gapps.yrdsb.ca>
2022-11-25 00:41:03 -0500

    Created elephant and made it move

---
+++ Elephant.java
@@ -0,0 +1,15 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * The elephant can move.
+ * 
+ * Stephen Liu 
+ * November 25, 2022
+ */
+public class Elephant extends Actor
+{
+    public void act()
+    {
+        move(7);
+    }
+}
--- MyWorld.java
+++ MyWorld.java
@@ -1,21 +1,18 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 /**
- * Write a description of class MyWorld here.
+ * This is where the elephant moves.
  * 
- * @author (your name) 
- * @version (a version number or a date)
+ * Stephen Liu
+ * November 25, 2022
  */
 public class MyWorld extends World
 {
-
-    /**
-     * Constructor for objects of class MyWorld.
-     * 
-     */
     public MyWorld()
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(600, 400, 1);
+        Elephant e=new Elephant();
+        addObject(e, 100, 200);
     }
 }
