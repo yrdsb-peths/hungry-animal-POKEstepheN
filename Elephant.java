e24974cfb6d126815f9a4c03bde34b55325d7de5
POKEstepheN <348921842@gapps.yrdsb.ca>
2022-12-07 14:23:52 -0500

    Creating, animating, and resizing Elephant images

--- Elephant.java
+++ Elephant.java
@@ -9,10 +9,22 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 public class Elephant extends Actor
 {
     GreenfootSound elephantSound=new GreenfootSound("elephantcub.mp3");
-    
+    GreenfootImage[] idle=new GreenfootImage[8];
     public Elephant()
     {
-        
+        for(int i=0; i<idle.length; i++)
+        {
+            idle[i]=new GreenfootImage("images/elephant_idle/idle"+i+".png");
+            idle[i].scale(100, 100);
+        }
+        setImage(idle[0]);
+    }
+    
+    int imageIndex=0;
+    public void animateElephant()
+    {
+        setImage(idle[imageIndex]);
+        imageIndex=(imageIndex+1)%idle.length;
     }
     
     public void act()
@@ -34,6 +46,7 @@ public class Elephant extends Actor
             setLocation(getX(), getY()+5);
         }
         eat();
+        animateElephant();
     }
     
     public void eat()
