12f5811356b01182366d1eb690696a9362847251
POKEstepheN <348921842@gapps.yrdsb.ca>
2022-12-07 14:45:23 -0500

    Make the elephant face left

--- Elephant.java
+++ Elephant.java
@@ -9,22 +9,42 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 public class Elephant extends Actor
 {
     GreenfootSound elephantSound=new GreenfootSound("elephantcub.mp3");
-    GreenfootImage[] idle=new GreenfootImage[8];
+    GreenfootImage[] idleRight=new GreenfootImage[8];
+    GreenfootImage[] idleLeft=new GreenfootImage[8];
+    
+    String facing = "right";
+    
     public Elephant()
     {
-        for(int i=0; i<idle.length; i++)
+        for(int i=0; i<idleRight.length; i++)
         {
-            idle[i]=new GreenfootImage("images/elephant_idle/idle"+i+".png");
-            idle[i].scale(100, 100);
+            idleRight[i]=new GreenfootImage("images/elephant_idle/idle"+i+".png");
+            idleRight[i].scale(100, 100);
         }
-        setImage(idle[0]);
+        
+        for(int i=0; i<idleLeft.length; i++)
+        {
+            idleLeft[i]=new GreenfootImage("images/elephant_idle/idle"+i+".png");
+            idleLeft[i].mirrorHorizontally();
+            idleLeft[i].scale(100, 100);
+        }
+        
+        setImage(idleRight[0]);
     }
     
     int imageIndex=0;
     public void animateElephant()
     {
-        setImage(idle[imageIndex]);
-        imageIndex=(imageIndex+1)%idle.length;
+        if(facing.equals("right"))
+        {
+            setImage(idleRight[imageIndex]);
+            imageIndex=(imageIndex+1)%idleRight.length;
+        }
+        else
+        {
+            setImage(idleLeft[imageIndex]);
+            imageIndex=(imageIndex+1)%idleLeft.length;
+        }
     }
     
     public void act()
@@ -32,10 +52,12 @@ public class Elephant extends Actor
         if(Greenfoot.isKeyDown("d"))
         {
             setLocation(getX()+5, getY());
+            facing="right";
         }
         if(Greenfoot.isKeyDown("a"))
         {
             setLocation(getX()-5, getY());
+            facing="left";
         }
         if(Greenfoot.isKeyDown("w"))
         {
