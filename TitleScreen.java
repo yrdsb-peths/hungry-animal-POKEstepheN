028778b4f93fc4a80e92e0d2acef826e1f300e87
POKEstepheN <348921842@gapps.yrdsb.ca>
2022-12-09 14:41:10 -0500

    Adding objects from the visual interface and saving the world

--- TitleScreen.java
+++ TitleScreen.java
@@ -12,10 +12,11 @@ public class TitleScreen extends World
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(600, 400, 1);
-        Label titleLabel=new Label("Hungry Elephant", 80);
-        addObject(titleLabel, getWidth()/2, getHeight()/2);
+        Label titleLabel=new Label("The Elephant", 80);
+        addObject(titleLabel, 250, 100);
+        prepare();
     }
-    
+
     public void act()
     {
         if(Greenfoot.isKeyDown("space"))
@@ -23,5 +24,16 @@ public class TitleScreen extends World
             MyWorld world=new MyWorld();
             Greenfoot.setWorld(world);
         }
+    }
+    
+    /**
+     * Prepare the world for the start of the program.
+     * That is: create the initial objects and add them to the world.
+     */
+    private void prepare()
+    {
+        Elephant elephant = new Elephant();
+        addObject(elephant,485,109);
+        elephant.setLocation(532,97);
     }
 }
